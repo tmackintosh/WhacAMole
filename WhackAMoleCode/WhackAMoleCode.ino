60af0205e278431abf09c06159b8d29ae87c0ef3
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2021-10-27 20:39:17 +0100

    Switch lights on at the end of the game

--- WhackAMoleCode/WhackAMoleCode.ino
+++ WhackAMoleCode/WhackAMoleCode.ino
@@ -9,6 +9,7 @@ int randNumber;
 int whiteLEDOn; 
 
 bool wasIncreased = false;
+bool completed = false;
 int score = 0;
 
 //setup interrupt, button input and LED outputs
@@ -24,36 +25,52 @@ void setup() {
 
 //run main program loop
 void loop() {
-  randNumber = random(3); // select a random number
-  digitalWrite(ledPin[randNumber], HIGH); // light the LED with this number
-  delay(delayTime);
-  digitalWrite(ledPin[randNumber], LOW);
-
-  if (wasIncreased == true) {
-    score += 1;
-    
-    Serial.print("Score! Current score:");
-    Serial.println(score);
-  } else if (randNumber == 1) {
-    Serial.print("Missed! Current score:");
-    Serial.println(score);
-  }
+  while (completed == false) {
+    randNumber = random(3); // select a random number
+    digitalWrite(ledPin[randNumber], HIGH); // light the LED with this number
+    delay(delayTime);
+    digitalWrite(ledPin[randNumber], LOW);
   
-  delay(delayTime);
+    if (wasIncreased == true) {
+      score += 1;
+      
+      Serial.print("Score! Current score:");
+      Serial.println(score);
 
-  wasIncreased = false;
-  whiteLEDOn = digitalRead(whiteLED);
-  if(whiteLEDOn=HIGH){
-    digitalWrite(whiteLED, LOW);
-  }  //if whiteLED on = turn it off
+      if (score == 1) {
+        Serial.println("You win!");
+        completed = true;
+      }
+    } else if (randNumber == 1) {
+      Serial.print("Missed! Current score:");
+      Serial.println(score);
+    }
+    wasIncreased = false;
+    
+    delay(delayTime);
+  
+    whiteLEDOn = digitalRead(whiteLED);
+    if(whiteLEDOn=HIGH){
+      digitalWrite(whiteLED, LOW);
+    }  //if whiteLED on = turn it off
+  }
+
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPin[i], HIGH);
+  }
+
+  delay(1000);
+
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPin[i], LOW);
+  }
 }
 
 void playerOneInput() {
   if (randNumber == 1) {
     wasIncreased = true;
+    digitalWrite(whiteLED, HIGH);
   }
-  
-  digitalWrite(whiteLED, HIGH); 
   /*change this code so that white LED only switches on when button is pressed 
   at the right time*/
 }
