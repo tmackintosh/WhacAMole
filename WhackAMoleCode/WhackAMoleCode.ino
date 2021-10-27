e94e234b38914c00a01b4f33aad898acea8f22c0
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2021-10-27 20:15:56 +0100

    Increase counter for each successful whack

--- WhackAMoleCode/WhackAMoleCode.ino
+++ WhackAMoleCode/WhackAMoleCode.ino
@@ -8,6 +8,9 @@ int delayTime = 1000; // time delay between lights on/off
 int randNumber;
 int whiteLEDOn; 
 
+bool wasIncreased = false;
+int score = 0;
+
 //setup interrupt, button input and LED outputs
 void setup() {
   Serial.begin(9600);
@@ -25,18 +28,31 @@ void loop() {
   digitalWrite(ledPin[randNumber], HIGH); // light the LED with this number
   delay(delayTime);
   digitalWrite(ledPin[randNumber], LOW);
+
+  if (wasIncreased == true) {
+    score += 1;
+    
+    Serial.print("Score! Current score:");
+    Serial.println(score);
+  } else if (randNumber == 1) {
+    Serial.print("Missed! Current score:");
+    Serial.println(score);
+  }
+  
   delay(delayTime);
+
+  wasIncreased = false;
   whiteLEDOn = digitalRead(whiteLED);
   if(whiteLEDOn=HIGH){
     digitalWrite(whiteLED, LOW);
   }  //if whiteLED on = turn it off
 }
 
-int count = 0;
-
 void playerOneInput() {
-  count += 1;
-  Serial.println(count);
+  if (randNumber == 1) {
+    wasIncreased = true;
+  }
+  
   digitalWrite(whiteLED, HIGH); 
   /*change this code so that white LED only switches on when button is pressed 
   at the right time*/
